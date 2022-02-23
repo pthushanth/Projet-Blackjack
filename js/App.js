8b5ffd779ad1253c9631dc45f8e2a8da2eb699ae
Kurunchi Chandra <ckurunchi@gmail.com>
2022-02-23 21:12:10 +0100

    design homepage

--- js/App.js
+++ js/App.js
@@ -8,6 +8,7 @@ const scoreDiv = document.getElementById("score");
 const scoreAfterDiv = document.getElementById("scoreAfter");
 const resultDiv = document.getElementById("result");
 const RemainingCardsDiv = document.getElementById("remainingCards");
+const play = document.getElementById("play");
 let newImg;
 let vibrateDuration;
 const game = await Game.create();
@@ -26,7 +27,7 @@ const hit = async () => {
     buttonStand.disabled = true;
     if (game.player.isWin) {
       vibrateDuration = [300, 100, 300, 100, 300];
-      resultDiv.innerHTML = "<h1 style='color:green'>You Won</h1>";
+      resultDiv.innerHTML = "<div id='open-modal-win' class='modal-window-win'><div><a href='#' title='Close' class='modal-close-win'>Close</a><h1>You Won !</h1><div><p>'You won because your score futur '" + game.player.scoreAfterHold + "' is over 21'</p></div></div></div>"
     } else {
       vibrateDuration = [
         100, 50, 100, 50, 100, 50, 100, 50, 100, 50, 100, 50, 100, 50, 100,
@@ -47,11 +48,11 @@ const hold = async () => {
     buttonTake.disabled = true;
     buttonStand.disabled = true;
     if (game.player.isWin) {
-      resultDiv.innerHTML = "<h1 style='color:green'>You Won</h1>";
-      scoreAfterDiv.innerHTML =
-        "You won because your score futur " +
-        game.player.scoreAfterHold +
-        " is over 21";
+      resultDiv.innerHTML = "<div id='open-modal-win' class='modal-window-win'><div><a href='#' title='Close' class='modal-close-win'>Close</a><h1>You Won !</h1><div><p>'You won because your score futur '" + game.player.scoreAfterHold + "' is over 21'</p></div></div></div>"
+      // scoreAfterDiv.innerHTML =
+      //   "You won because your score futur " +
+      //   game.player.scoreAfterHold +
+      //   " is over 21";
     } else {
       resultDiv.innerHTML = "<h1 style='color:red'>You Lost</h1>";
       scoreAfterDiv.innerHTML =
@@ -115,3 +116,10 @@ document.addEventListener("keypress", function (e) {
     }
   }
 });
+
+function load () {
+  setTimeout( () => {
+    $(loader).
+  }, 3000);
+ 
+}
